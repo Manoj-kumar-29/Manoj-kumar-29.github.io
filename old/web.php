eaf4b4a2ccb4d9d87d3265334178cfb773b91ace
MANOJ KUMAR <kumarmanojmp29@gmail.com>
2021-04-22 02:13:59 +0530

    cm

---
+++ old/web.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tabel with data base</title>
+	<style >
+		table{
+			border-collapse: collapse;
+			width: 100%;
+			color: #588c7e;
+			font-family: monospace;
+			font-size: 25px;
+			text-align: center;
+		}
+		th{
+			background-color: #588c7e;
+			color: white;
+		}
+		tr:nth-child(even) {
+			background-color: #f2f2f2
+		}
+	</style>
+</head>
+<body>
+	<table>
+	 <tr>
+	 	<th>Sl.no</th>
+	 	<th>Name</th>
+	 	<th>Email</th>
+	 	<th>Amount</th>
+	 </tr>
+	 <?php
+	 $conn = mysql_connect("localhost", "root", "", "details");
+	 if($conn-> conncet_error){
+	 	dei("Connection failed:". $conn-> connect_error);
+	 }
+
+	 $sql = "SELECT SlNO,NAME,MAIL ID,AMOUNT FROM login";
+	 $result = $conn-> query($sql);
+
+	 if ($result-> num_rows > 0){
+	 	while ($row = $result-> fetch_assoc()){
+	 		echo "<tr><td>". $row["SlNO"] "<tr><td>". $row["NAME"]"<tr><td>". $row["MAIL ID"]"<tr><td>". $row["AMOUNT"] ."</td></tr>";
+	 	}
+	 	echo "</table>";
+
+	 }
+      else{
+      	echo "0 result";
+      }
+
+      $conn-> close();
+
+	 ?>
+	</table>
+
+</body>
+</html>
