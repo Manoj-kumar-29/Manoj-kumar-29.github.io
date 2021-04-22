a9738b452f6034dc348e0570d06c079f8ed41483
MANOJ KUMAR <kumarmanojmp29@gmail.com>
2021-04-22 17:22:56 +0530

    55

--- old/web (6).php
+++ old/web (6).php
@@ -29,17 +29,17 @@
 	 	<th>Amount</th>
 	 </tr>
 	 <?php
-	 $conn = mysql_connect("localhost", "root", "", "details");
+	 $conn = mysqli_connect("localhost", "root", "", "details")
 	 if($conn-> conncet_error){
-	 	dei("Connection failed:". $conn-> connect_error);
+	 	die("Connection failed:". $conn-> connect_error);
 	 }
 
-	 $sql = "SELECT * FROM "bankdetails";
+	 $sql = ("SELECT SlNO,NAME,MAIL ID,AMOUNT FROM login");
 	 $result = $conn-> query($sql);
 
 	 if ($result-> num_rows > 0){
-	 	while ($row = $result-> fetch_assoc()){
-	 		echo "<tr><td>". $row["Sl.NO"] "<tr><td>". $row["NAME"]"<tr><td>". $row["MAIL ID"]"<tr><td>". $row["AMOUNT"] ."</td></tr>";
+	 	while{
+	 	echo "<tr><td>". $row["Sl.NO"] ."</td><td>". $row["NAME"]."</td><td>".$row["MAIL ID"]."</td><td>".$row["BALANCE"]."</td><td>"."</td></tr>";
 	 	}
 	 	echo "</table>";
 
--- old/web.php
+++ old/web.php
@@ -2,23 +2,7 @@
 <html>
 <head>
 	<title>Tabel with data base</title>
-	<style >
-		table{
-			border-collapse: collapse;
-			width: 100%;
-			color: #588c7e;
-			font-family: monospace;
-			font-size: 25px;
-			text-align: center;
-		}
-		th{
-			background-color: #588c7e;
-			color: white;
-		}
-		tr:nth-child(even){
-			background-color: #f2f2f2;
-		}
-	</style>
+	
 </head>
 <body>
 	<table>
@@ -34,7 +18,7 @@
 	 	dei("Connection failed:". $conn-> connect_error);
 	 }
 
-	 $sql = "SELECT * FROM "bankdetails";
+	 $sql = "SELECT SlNO,NAME,MAIL ID,AMOUNT FROM login";
 	 $result = $conn-> query($sql);
 
 	 if ($result-> num_rows > 0){
